694e1e81b6d12f5efaab9de911bbffcdf81df0bc
Nata R <natasha.melanis@yandex.ru>
2023-03-30 00:54:58 +0300

    Bonus 625

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
-        int price = 24_000;
+        int price = 12_500;
         int miles = service.calculate(price); // должно получиться 500
         System.out.println(miles);
     }
